79a448c0b3fb8e16276408d7d8ebeac5a6e2ff44
Marcos-dorado <marcosdoradoh@gmail.com>
2024-07-29 08:25:04 -0500

    segundo commit para que funcione el modal

--- src/index.js
+++ src/index.js
@@ -8,6 +8,15 @@ const port = process.env.PORT || 3300;
 
 // middlewre
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+
 app.use(express.json())
 app.use("/api",userRoutes);
 
